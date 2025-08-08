2b360f96517808da8921f06aa9f4df4e028f6318
yonghai <helloyyh2022@outlook.com>
2025-08-08 16:49:11 +0800

    修复

--- frontend/src/config/menuConfig.jsx
+++ frontend/src/config/menuConfig.jsx
@@ -102,7 +102,6 @@ export const menuItems = [
       roles: ['owner'],
     },
     children: [
-      { label: '团队任务监控', path: 'team_dashboard', icon: 'contact' },
       { label: '销售数据', path: '/sales_data', icon: 'bar-chart-3' },
       { label: '美工数据', path: '/art_data', icon: 'bar-chart-3' },
     ]
