2369fab9868372cdc8e3838d70688c5d2c01708e
QueDao <your-email@example.com>
2025-09-08 17:31:04 +0800

    修复导出客户功能

--- frontend/src/pages/OnlineClientPage.jsx
+++ frontend/src/pages/OnlineClientPage.jsx
@@ -113,7 +113,7 @@ const OnlineClientPage = () => {
         }
     };
     // 导出客户
-    const [exportLoading,setExportLoading] = useState(false);
+    const [exportLoading, setExportLoading] = useState(false);
 
     const handleExport = async (exportFilters) => {
         setExportLoading(true);
